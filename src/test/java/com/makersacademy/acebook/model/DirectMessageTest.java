72f34b4cbfbc70f688c6d2c18ff98fda157555ea
Alexandre Monoyioudis <alexandremonoyioudis@gmail.com>
2024-11-26 14:17:33 +0000

    Update DirectMessageTest.java

--- src/test/java/com/makersacademy/acebook/model/DirectMessageTest.java
+++ src/test/java/com/makersacademy/acebook/model/DirectMessageTest.java
@@ -12,7 +12,7 @@ public class DirectMessageTest {
     private DirectMessage directMessage = new DirectMessage("hi how are you", "1", "2", LocalDateTime.now());
     @Test
     public void sendDirectMessage() {
-        // assertThat(directMessage.getContent(), containsString("hi how are you")); 
+        assertThat(directMessage.getContent(), containsString("hi how are you")); 
     }
 
 }
