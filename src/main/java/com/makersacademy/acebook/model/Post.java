5cbb5e1f0fd370365f76026c38a87edb61cf7f8b
Alexandre Monoyioudis <alexandremonoyioudis@gmail.com>
2024-11-26 14:09:31 +0000

    post tests working

--- src/main/java/com/makersacademy/acebook/model/Post.java
+++ src/main/java/com/makersacademy/acebook/model/Post.java
@@ -1,25 +1,30 @@
 package com.makersacademy.acebook.model;
 
 import jakarta.persistence.*;
-
+import java.time.LocalDateTime;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
 
-@Data
 @Entity
+@Getter @Setter @NoArgsConstructor
 @Table(name = "POSTS")
 public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String user_id;
     private String content;
+    private boolean friends_only;
+    private LocalDateTime date_time;
 
-    public Post() {}
-
-    public Post(String content) {
+    public Post(String user_id, String content, Boolean friends_only, LocalDateTime date_time) {
+        this.user_id = user_id;
         this.content = content;
+        this.friends_only = friends_only;
+        this.date_time = date_time;
     }
-    public String getContent() { return this.content; }
-    public void setContent(String content) { this.content = content; }
 
 }
--- src/test/java/com/makersacademy/acebook/model/DirectMessageTest.java
+++ src/test/java/com/makersacademy/acebook/model/DirectMessageTest.java
@@ -1,7 +1,6 @@
 package com.makersacademy.acebook.model;
 
 import org.junit.jupiter.api.Test;
-
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
 import java.time.LocalDateTime;
@@ -12,7 +11,7 @@ public class DirectMessageTest {
     private DirectMessage directMessage = new DirectMessage("hi how are you", "1", "2", LocalDateTime.now());
     @Test
     public void sendDirectMessage() {
-        // assertThat(directMessage.getContent(), containsString("hi how are you")); 
+        assertThat(directMessage.getContent(), containsString("hi how are you"));
     }
 
 }
