ebc912ecd5d6178c1d2c061e2ebbea1d9149c8ad
Daniel Sussman <daniel.o.sussman@gmail.com>
2024-11-26 12:26:37 +0000

    changes to user model

---
+++ src/main/java/com/makersacademy/acebook/controller/DirectMessagesController.java
@@ -0,0 +1,18 @@
+package com.makersacademy.acebook.controller;
+
+import com.makersacademy.acebook.model.Post;
+import com.makersacademy.acebook.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class DirectMessagesController {
+    @GetMapping("/conversations")
+    public String index(Model model) {
+        return "direct_messages/index";
+    }
+}
--- src/main/java/com/makersacademy/acebook/model/User.java
+++ src/main/java/com/makersacademy/acebook/model/User.java
@@ -2,34 +2,51 @@ package com.makersacademy.acebook.model;
 
 import jakarta.persistence.*;
 
-import lombok.Data;
+import javassist.LoaderClassPath;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 import static java.lang.Boolean.TRUE;
 
-@Data
 @Entity
+@Getter
+@Setter
 @Table(name = "USERS")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String username;
+    private String userId;
     private boolean enabled;
+    private String nickname;
+    private LocalDateTime lastLogin;
+
+    public User(String userId, String nickname, LocalDateTime lastLogin) {
+        this.userId = userId;
+        this.enabled = TRUE;
+        this.nickname = nickname;
+        this.lastLogin = lastLogin;
+    }
+
+    public User(String userId, boolean enabled, String nickname, LocalDateTime lastLogin) {
+        this.userId = userId;
+        this.enabled = enabled;
+        this.nickname = nickname;
+        this.lastLogin = lastLogin;
+    }
 
     public User() {
-        this.enabled = TRUE;
+        // Default constructor required by Hibernate
     }
 
-    public User(String username) {
-        this.username = username;
-        this.enabled = TRUE;
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
     }
 
-    public User(String username, boolean enabled) {
-        this.username = username;
-        this.enabled = enabled;
+    public void setLastLogin(LocalDateTime lastLogin) {
+        this.lastLogin = lastLogin;
     }
-
-    public String getUsername() { return this.username; }
-    public void setUsername(String username) { this.username = username; }
 }
