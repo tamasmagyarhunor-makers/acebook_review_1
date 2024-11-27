bac3a88e8415292c60bf4e32417416d93b582663
Alexandre Monoyioudis <alexandremonoyioudis@gmail.com>
2024-11-27 12:46:20 +0000

    Update and rename AddFriendRequest.java to FriendRequest.java

--- src/main/java/com/makersacademy/acebook/model/FriendRequest.java
+++ src/main/java/com/makersacademy/acebook/model/FriendRequest.java
@@ -10,11 +10,11 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @Table(name = "friend_requests")
-public class friend_requests {
+public class FriendRequest {
     private String requesting_user;
     private String requested_user;
 
-    public friend_requests(String requesting_user, String requested_user) {
+    public FriendRequest(String requesting_user, String requested_user) {
         this.requesting_user = requesting_user;
         this.requesting_user = requested_user;
     }
