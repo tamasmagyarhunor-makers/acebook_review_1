33ad7db92fe8932d06d148b037c1cc07cbad2381
God'smercy Oluwanusin <gmercyoluwanusin@gmail.com>
2024-11-28 14:35:47 +0000

    Changing comment model, table, repository, and deleting commentID so user can make multiple comments on a post.

--- src/main/java/com/makersacademy/acebook/repository/CommentRepository.java
+++ src/main/java/com/makersacademy/acebook/repository/CommentRepository.java
@@ -1,15 +1,13 @@
 package com.makersacademy.acebook.repository;
 
-import com.makersacademy.acebook.dto.CommentId;
 import com.makersacademy.acebook.model.Comment;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
-import java.util.Optional;
 
-public interface CommentRepository extends CrudRepository<Comment, CommentId> {
+public interface CommentRepository extends CrudRepository<Comment, Long> {
     List<Comment> findByUserId(String userId);
     List<Comment> findByPostId(Long postId);
-    Optional<Comment> findByUserIdAndPostId(String userId, Long postId);
+    List<Comment> findByUserIdAndPostId(String userId, Long postId);
 
 }
