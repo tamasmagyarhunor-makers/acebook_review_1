392216cfd03de3948d880a2f00067423a9b89152
Alexandre Monoyioudis <alexandremonoyioudis@gmail.com>
2024-11-29 16:29:34 +0000

    added playwright to workflows

--- src/test/java/com/makersacademy/acebook/feature/SignUpTest.java
+++ src/test/java/com/makersacademy/acebook/feature/SignUpTest.java
@@ -34,14 +34,14 @@ public class SignUpTest {
         page.setDefaultTimeout(5000);
         page.getByText("Sign up").click();
         String email = faker.name().username() + "@email.com";
-//
+
 //        page.screenshot(new Page.ScreenshotOptions()
 //                .setPath(Paths.get("screenshot0.png"))
 //                .setFullPage(true));
 
         page.locator("#email").fill(email);
         page.locator("#password").fill("P@s5W0rd");
-//
+
 //        page.screenshot(new Page.ScreenshotOptions()
 //                .setPath(Paths.get("screenshot1.png"))
 //                .setFullPage(true));
