277ef543d33147c7ecad13c4feef284dcc7f8fc5
Alexandre Monoyioudis <alexandremonoyioudis@gmail.com>
2024-11-29 17:39:43 +0000

    added playwright to workflows

--- src/test/java/com/makersacademy/acebook/feature/SignUpTest.java
+++ src/test/java/com/makersacademy/acebook/feature/SignUpTest.java
@@ -1,29 +1,26 @@
 package com.makersacademy.acebook.feature;
 
 import com.github.javafaker.Faker;
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserType;
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.*;
 import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.nio.file.Paths;
 
 public class SignUpTest {
 
     Page page;
     Faker faker;
     Playwright playwright;
+    BrowserContext context;
 
     @BeforeEach
     public void setup() {
         faker = new Faker();
         playwright = Playwright.create();
         Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
-        page = browser.newPage();
+        context = browser.newContext();
+        page = context.newPage();
         page.navigate("http://localhost:8080/");
     }
 
