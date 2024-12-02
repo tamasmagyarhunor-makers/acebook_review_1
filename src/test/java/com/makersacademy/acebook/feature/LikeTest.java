76eaf119517ac49fdd06c033cc5a946f2ef43c4e
Alexandre Monoyioudis <alexandremonoyioudis@gmail.com>
2024-12-02 14:37:45 +0000

    adds likesTest

---
+++ src/test/java/com/makersacademy/acebook/feature/LikeTest.java
@@ -0,0 +1,53 @@
+package com.makersacademy.acebook.feature;
+
+import com.github.javafaker.Faker;
+import com.microsoft.playwright.*;
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class LikeTest {
+
+    Page page;
+    Faker faker;
+    Playwright playwright;
+    BrowserContext context;
+
+    @BeforeEach
+    public void setup() {
+        faker = new Faker();
+        playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
+        context = browser.newContext();
+        page = context.newPage();
+        page.navigate("http://localhost:8080/");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        playwright.close();
+    }
+
+    @Test
+    public void like(){
+        page.setDefaultTimeout(6000);
+        page.getByText("Sign up").click();
+        String email = faker.name().username() + "@email.com";
+
+//        page.screenshot(new Page.ScreenshotOptions()
+//                .setPath(Paths.get("screenshot0.png"))
+//                .setFullPage(true));
+
+        page.locator("#email").fill(email);
+        page.locator("#password").fill("P@s5W0rd");
+
+//        page.screenshot(new Page.ScreenshotOptions()
+//                .setPath(Paths.get("screenshot1.png"))
+//                .setFullPage(true));
+
+        page.getByText("Continue").nth(1).click();
+        page.getByText("Accept").click();
+
+    }
+}
